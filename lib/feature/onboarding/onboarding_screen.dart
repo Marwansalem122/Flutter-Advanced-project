d500ba5870c357b5f21b4a69b98cc8aea05b5725
marwan salem <marwansalem01274087424@gmail.com>
2024-08-12 15:38:58 +0300

    solve some problem in previous push

--- lib/feature/onboarding/onboarding_screen.dart
+++ lib/feature/onboarding/onboarding_screen.dart
@@ -1,4 +1,9 @@
+import 'package:complete_flutter_project/core/theming/styles.dart';
+import 'package:complete_flutter_project/feature/onboarding/widgets/doc_logo_and_name.dart';
+import 'package:complete_flutter_project/feature/onboarding/widgets/doctor_image_and_text.dart';
+import 'package:complete_flutter_project/feature/onboarding/widgets/get_started_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
@@ -6,9 +11,37 @@ class OnBoardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("OnBoarding Screen"),
+        body: SafeArea(
+            child: SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.only(top: 20.h, bottom: 10.h),
+        child: Column(
+          children: [
+            const DocLogoAndName(),
+            SizedBox(
+              height: 30.h,
+            ),
+            const DoctorImageAndText(),
+            SizedBox(
+              height: 30.h,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 41.4.w),
+              child: Column(children: [
+                Text(
+                  "Manage and schedule all of your medical appointments easily with Docdoc to get a new experience.",
+                  style: TextStyles.font13GreyRegular,
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 30.h,
+                ),
+                const GetStartedButton()
+              ]),
+            )
+          ],
+        ),
       ),
-    );
+    )));
   }
 }
